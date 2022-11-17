d90136752a7443d0ce6af02095eee461dafdd31b
julia <juliaborisova056@gmail.com>
2022-11-17 19:09:40 +0200

    Uploading homework.

--- JS-Advanced/DOM-manipulations-and-events-lab/02.delete_from_table/app.js
+++ JS-Advanced/DOM-manipulations-and-events-lab/02.delete_from_table/app.js
@@ -1,3 +1,20 @@
 function deleteByEmail() {
+
+    let email = document.getElementsByName('email')[0].value;
+
+    let tableCells = document
+        .querySelectorAll('#customers td:nth-child(2)');
+
+    let found = false;
+
+    for (const cell of tableCells) {
+        if (cell.textContent == email) {
+            cell.parentElement.remove();
+            found = true;
+        }
+    }
     
+    let result = document.getElementById('result');
+    
+    result.textContent = found ? 'Deleted.' : 'Not found.';
 }
